3018b53229f958d08ffc60d613e966d127bc721d
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-14 19:58:27 +0200

    Models

--- designPattern/architectureMVC/exemple/main.py
+++ designPattern/architectureMVC/exemple/main.py
@@ -1,6 +1,8 @@
 """Entry point."""
 
+from models.card import Card
 from models.deck import Deck
+from models.player import Player
 
 from controllers.base import Controller
 from controllers.evaluate import CheckerRankAndSuitIndex
@@ -12,7 +14,18 @@ from views.internet import InternetStreamingView
 
 
 def main():
+    card1 = Card("diamonds", "cinq")
+    card2 = Card("coeurs", "cinq")
+    card3 = Card("coeurs", "valet")
+    print(card1 < card2)
+    print(card3 > card2)
+
     deck = Deck()
+    print(deck)
+
+    player = Player("John")
+    player.hand.append(deck.draw_card())
+    print(player.hand)
 
     active_view = PlayerView()
     passive_views = (active_view, BroadcastView(), InternetStreamingView())
@@ -24,5 +37,5 @@ def main():
     game.run()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     main()
